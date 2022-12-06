96fb61f5c9926d9e2517e77449e8acbe560dafea
Dementan <masav7986@gmail.com>
2022-12-06 20:09:57 +0300

    The  user enters the side of the square. Calculate area and perimeter

---
+++ main.py
@@ -0,0 +1,5 @@
+# The  user enters the side of the square. Calculate area and perimeter
+
+side = int(input("Enter a side of a square: "))
+print(side**2)  # formula of a square
+print(side*4)   # formula of a perimeter
